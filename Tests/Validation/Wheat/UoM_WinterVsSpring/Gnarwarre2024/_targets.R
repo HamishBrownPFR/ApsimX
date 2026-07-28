a72ede401fd162228acf34494358739058667077
Edmar Teixeira <Edmar.Teixeira@plantandfood.co.nz>
2026-07-28 17:09:38 +1200

    Improve name fix function with universal solution prior to name corrections - Gna24 tested

--- Tests/Validation/Wheat/UoM_WinterVsSpring/Gnarwarre2024/_targets.R
+++ Tests/Validation/Wheat/UoM_WinterVsSpring/Gnarwarre2024/_targets.R
@@ -24,7 +24,7 @@ targets::tar_source("../targets_MasterScripts")
 
 # Load local corrections specific to this Gna24 dataset
 source("R/apply_corrections_Gna24.R")
-source("R/apply_name_corrections_Gna24.R")
+#source("R/apply_name_corrections_Gna24.R")
 # ------------------------------------------------------------------------------
 # 3. PROJECT DEFINITION
 # ------------------------------------------------------------------------------
@@ -128,7 +128,8 @@ list(
   ),
   tar_target(
     name = df_obs_renamed,
-    command = apply_name_corrections_Gna24(
+    #command = apply_name_corrections_Gna24(
+    command = rename_rescale_obs_vars(
       df_obs           = df_obs_raw,
       mapping_csv_path = track_mapping_csv
     )
